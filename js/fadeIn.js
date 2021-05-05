9f72ccbce9f5e10cae39930628707be7a65f988f
Alex Novak <alexn@celebratedrupal.com>
2021-05-05 14:48:18 +0300

    Change fadein effect

--- js/fadeIn.js
+++ js/fadeIn.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+    $('.fadeinAfterLoad').delay(1000).animate({'opacity':'1'},1000);
     $(window).scroll( function(){
         $('.fadein').each( function(i){
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_element = $(this).offset().top + ($(this).outerHeight()/4);
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if( bottom_of_window > bottom_of_element ){
                 $(this).animate({'opacity':'1'},1000);
