b71c5746b784477545f9ff16702f4168e7c2db94
Alex Novak <alexn@celebratedrupal.com>
2021-05-05 15:04:19 +0300

    moved external js to esteemed.js file

--- js/esteemed.js
+++ js/esteemed.js
@@ -14097,3 +14097,16 @@ $('#lead_submission').on('submit', function(e) {
       });
   });
 });
+
+$(document).ready(function() {
+  $('.fadeinAfterLoad').delay(1000).animate({'opacity':'1'},1000);
+  $(window).scroll( function(){
+    $('.fadein').each( function(i){
+      var bottom_of_element = $(this).offset().top + ($(this).outerHeight()/4);
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      if( bottom_of_window > bottom_of_element ){
+        $(this).animate({'opacity':'1'},1000);
+      }
+    });
+  });
+});
